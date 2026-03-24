860004d308ea9c0d82bdb151fca66916086d82cb
Hamza <hamzaalhindi2006@gmail.com>
2026-03-24 17:44:03 -0500

    Created groundwork for UDP connection in utils

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,6 +1,9 @@
 #pragma once
 #include <openssl/evp.h>
 #include <iomanip>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 std::string sha1(const std::string& data, size_t start, size_t length) {
     unsigned char hash[20];
@@ -11,4 +14,46 @@ std::string sha1(const std::string& data, size_t start, size_t length) {
     EVP_DigestFinal_ex(ctx, hash, &hash_len);
     EVP_MD_CTX_free(ctx);
     return std::string(reinterpret_cast<char*>(hash), 20);
+}
+
+int createUDPIpv4Socket() {
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd == -1)
+        throw std::runtime_error(std::string("socket: ") + strerror(errno));
+    return fd;
+}
+
+struct TrackerAddress {
+    std::string host;
+    std::string port;
+};
+
+TrackerAddress parse_tracker_url(const std::string& url) {
+    if (url.substr(0, 6) != "udp://")
+        throw std::runtime_error("Only UDP trackers supported: " + url);
+
+    std::string rest = url.substr(6);
+
+    size_t colon = rest.rfind(':');
+    if (colon == std::string::npos)
+        throw std::runtime_error("No port in tracker URL: " + url);
+
+    return {
+        rest.substr(0, colon),
+        rest.substr(colon + 1)
+    };
+}
+
+struct sockaddr_in createIPv4Address(const char* ip, int port) {
+    struct sockaddr_in address = {0};
+
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+
+    if (ip == NULL || ip[0] == '\0')
+        address.sin_addr.s_addr = INADDR_ANY;
+    else
+        inet_pton(AF_INET, ip, &address.sin_addr);
+
+    return address;
 }
