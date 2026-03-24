50661ae790a786d6ac6b5c5f5170707760414302
Hamza <hamzaalhindi2006@gmail.com>
2026-03-24 15:10:31 -0500

    .

---
+++ src/utils.hpp
@@ -0,0 +1,13 @@
+#include <openssl/evp.h>
+#include <iomanip>
+
+std::string sha1(const std::string& data, size_t start, size_t length) {
+    unsigned char hash[20];
+    EVP_MD_CTX* ctx = EVP_MD_CTX_new();
+    EVP_DigestInit_ex(ctx, EVP_sha1(), nullptr);
+    EVP_DigestUpdate(ctx, data.data() + start, length);
+    unsigned int hash_len;
+    EVP_DigestFinal_ex(ctx, hash, &hash_len);
+    EVP_MD_CTX_free(ctx);
+    return std::string(reinterpret_cast<char*>(hash), 20);
+}
